b93ccd93ada0921fd0d1d72cbf3e8d0e1e53d1da
Sergei Neimushchev <srgynmv@gmail.com>
2018-12-22 21:53:46 +0300

    Added initial routes for device

--- Server/routes/devices.js
+++ Server/routes/devices.js
@@ -5,17 +5,79 @@ const deviceController = require('../controllers/device')
 const router = express.Router()
 
 router.get('/', function(req, res) {
-    res.end('devices')
     const devicesQuery = deviceController.findAll()
-    log.debug(devices)
-    // for (let device of devices) {
-    //     log.debug(device)
-    // }
+    
+    devicesQuery.exec((err, devices) => {
+        if (err) {
+            log.error(err)
+            return
+        }
+
+        let response = []
+        for (let device of devices) {
+            response.push({
+                id: device.deviceId,
+                name: device.name
+            })
+        }
+
+        res.json(response)
+    })
 })
 
-router.get('/:deviceId', function(req, res) {
-    res.end('devices/id')
-    log.debug('/id')
+router.get('/:deviceId/actions', function(req, res) {
+    const deviceId = req.params.deviceId
+    const devicesQuery = deviceController.findOne(deviceId)
+    
+    devicesQuery.exec((err, device) => {
+        if (err) {
+            log.error(err)
+            return
+        }
+
+        if (!device) {
+            log.error(`Device ${deviceId} not found!`)
+            return
+        }
+
+        let result = []
+        for (let action of device.actions) {
+            result.push({
+                id: action.id,
+                name: action.name
+            })
+        }
+
+        res.json(result)
+    })
+})
+
+router.get('/:deviceId/sensors', function(req, res) {
+    const deviceId = req.params.deviceId
+    const devicesQuery = deviceController.findOne(deviceId)
+    
+    devicesQuery.exec((err, device) => {
+        if (err) {
+            log.error(err)
+            return
+        }
+
+        if (!device) {
+            log.error(`Device ${deviceId} not found!`)
+            return
+        }
+
+        let result = []
+
+        for (let sensor of device.sensors) {
+            result.push({
+                id: sensor.id,
+                name: sensor.type
+            })
+        }
+
+        res.json(result)
+    })
 })
 
 module.exports = router
